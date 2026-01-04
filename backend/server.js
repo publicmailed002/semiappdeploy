938d3e67a2268f2f408c57a6a167ce1fe14862da
publicmailed002 <mondarefacileter@gmail.com>
2026-01-04 17:44:53 +0100

    signup controller add

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,28 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from './src/authe.rout.js';
+import { connectDB } from './src/lib/db.js';
+dotenv.config();
 
 const app = express();
-
-app.get('/api/server', (req, res) => {
-  res.json({ message: 'Server endpoint running locally' });
-});
-
-// Keep the hello route for parity with the serverless function
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from backend on Vercel (local server)' });
-});
-
+app.use(express.json());
 const port = process.env.PORT || 3001;
+
+// app.get('/api/server', (req, res) => {
+//   res.json({ message: 'Server endpoint running locally' });
+// });
+
+// // Keep the hello route for parity with the serverless function
+// app.get('/api/hello', (req, res) => {
+//   res.json({ message: 'Hello from backend on Vercel (local server)' });
+// });
+
+
+app.use('/api/auth',authRoutes);
+
+
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`Backend server listening on http://localhost:${port}`);
+  connectDB();
 });
