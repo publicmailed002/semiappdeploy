dbeb04901c7b6b06b493bccbea2a4639496251dd
publicmailed002 <mondarefacileter@gmail.com>
2026-01-12 18:29:51 +0100

    chat continer setup done

--- backend/api/server.js
+++
@@ -1,5 +0,0 @@
-// Vercel-compatible serverless handler
-// GET /api/server
-export default function handler(req, res) {
-  res.status(200).json({ message: 'Server endpoint running on Vercel' });
-}
--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,10 +1,56 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { useChatStore } from '../store/useChatStore'
+import { useAuthStore } from '../store/useAuthStore'
+import ChatHader from './ChatHader';
+import NoChatHistoryPlaceHolder from '../components/NoChatHistoryPlaceHolder';
+import MessageInput from './MessageInput';
+import MeesgesLodingSkelton from './MeesgesLodingSkelton';
 function ChatContainer() {
+
+  const {selectedUser , getMessagesByUserId ,messages ,isMessagesLoding} = useChatStore();
+  const {authUser}  = useAuthStore();
+
+  useEffect(()=>{
+      getMessagesByUserId(selectedUser._id)
+  },[selectedUser ,getMessagesByUserId])
   return (
-    <div>
-      
-    </div>
+    <>
+     <ChatHader/>
+     <div className='flex-1 px-6 overflow-y-auto py-8'>
+
+      {messages.length > 0  && !isMessagesLoding? (
+       <div className='max-w-3xl mx-auto space-y-6'>
+
+        {messages.map((msg) =>(
+          <div key={msg._id}
+           className={`chat ${msg.senderId === authUser._id ? "chat-end" : "chat-start"}`}
+          > 
+
+           <div className={`chat-bubble relative ${msg.senderId === authUser._id ? "bg-cyan-600 text-white" :" bg-slate-800 text-slate-200"}`}>
+
+                {msg.image && (
+                  <img src={msg.image} alt='Shared' className='rounded-lg h-48 object-cover' />
+                )}
+                {msg.text && <p className='mt-2'> {msg.text }</p>}
+
+                <p className='text-xs mt-1 opacity-75 flex items-center gap-1'>
+                  {new Date(msg.createdAt).toISOString().slice(11,16)}
+                </p>
+
+           </div>
+
+
+          </div>
+        ))}
+
+       </div>
+      ): isMessagesLoding ? <MeesgesLodingSkelton/> :(
+        <NoChatHistoryPlaceHolder name={selectedUser.FullName} />
+      )}
+
+     </div>
+     <MessageInput/>
+    </>
   )
 }
 
